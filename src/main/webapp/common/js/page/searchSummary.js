da60e98d307b284fd095be93d4ff09282078c44a
suntt <suntt@github.com>
2017-09-28 11:05:02 +0800

    后评估页面功能开发

--- src/main/webapp/common/js/page/searchSummary.js
+++ src/main/webapp/common/js/page/searchSummary.js
@@ -10,8 +10,6 @@ $(function(){
 	// 初始化url趋势图
 	var urlTrendChart = null;
 	init_visit();
-
-
 });
 
 // 查询条件切换
@@ -34,7 +32,7 @@ function searchSubmit() {
     ajaxModuleUrl();
 }
 
-// 初始化时间
+// 初始化时间（默认查询前三个月）
 function initDateCondition(){
     var year,month;
     var currDate = new Date();
@@ -130,15 +128,40 @@ function ajaxModuleUrl() {
         searchVal = $("#searchUrl").val();
     }
     if(searchVal == null || searchVal == ""){
-        alert("查询条件不可为空！");
+        $("#infoModal").modal('show');
         return;
     }
     $("#searchVal").text("【"+cutStr(searchVal,50)+"】");
 	if(searchType == "url"){
         ajaxUrlData(searchVal);
 	}else if(searchType == "module"){
-
+        ajaxModuleData(searchVal);
 	}
+}
+
+// 刷新当前模块趋势数据（请求模块接口数据，使用url图表展示）
+function ajaxModuleData(module){
+    var startDate = $("#startDate").val();
+    var endDate = $("#endDate").val();
+    var p1 = "module=API&method=Actions.getPageTitle&pageName="+module+"&idSite="+idSite+"&period=range&date="+startDate+","+endDate+"&flat=1&format=JSON&token_auth="+t;
+    var p2 = "module=API&method=Actions.getPageTitle&pageName="+module+"&idSite="+idSite+"&period=day&date="+startDate+","+endDate+"&flat=1&format=JSON&token_auth="+t;
+    var urls = new Array();
+    urls.push(encodeURI(p1));
+    urls.push(encodeURI(p2));
+    var p = getBulkRequestParam(urls);
+    ajax_jsonp(piwik_url,p,function(data){
+        // 展示module视图
+        $("#noData,#searchModuleData").hide();
+        $("#searchUrlData,.main-content").show();
+        urlTrendChart.resize();
+        data = eval(data);
+        urlSummary = data[0];
+        urlDetail = data[1];
+        ana_visit_summary(); // 加载模块概览图
+        ana_visit();// 加载模块趋势图
+        anaCsTable(); // 加载访客数据表
+        ajaxModuleMind(module); // 加载模块上下游数据
+    });
 }
 
 // URL统计方法start
@@ -163,6 +186,7 @@ function ajaxUrlData(url){
 		ana_visit_summary(); // 加载url概览图
 		ana_visit();// 加载url趋势图
 		anaCsTable(); // 加载访客数据表
+        ajaxUrlMind(url); // 加载url上下游数据
 	});
 }
 
@@ -387,9 +411,201 @@ function initCsTable(csData){
 		"headers": ["日期", "浏览量", "唯一页面浏览量", "平均访问时长", "跳出率","退出率","页面性能响应(秒)"], //必须
 		"customHeader" : "<thead><tr><th rowspan='2'>日期</th><th colspan='2'>网站基础指标</th><th colspan='4'>流量质量指标</th></tr><tr><th>浏览量</th><th>唯一页面浏览量</th><th>平均访问时长</th><th>跳出率</th><th>退出率</th><th>页面性能响应(秒)</th></tr></thead>", // 自定义表头，若定义则覆盖默认表头
 		"data": csData.reverse(), //必须
-		"displayNum": 15, //必须  默认 10
+		"displayNum": 10, //必须  默认 10
 		"groupDataNum": 9 //可选  默认 10
 	});
 }
 // url指标详情表end
 // URL统计方法end
+
+// 页面上下游start
+
+// 请求模块上下游数据
+function ajaxModuleMind(module){
+    $("#jsmind_container").html("");
+    var startDate = $("#startDate").val();
+    var endDate = $("#endDate").val();
+    var param = {date:startDate+','+endDate,actionType:'title',actionName:module,format:'json',module:'API',method:'Transitions.getTransitionsForAction',filter_limit:-1,idSite:idSite,period:'range',token_auth:t};
+    ajax_jsonp(piwik_url,param,function(data){
+        data = eval(data);
+        if(data.hasOwnProperty("result") && data.result == "error"){
+            $("#jsmind_container").html("<div style='padding: 300px 0 0 400px;'>本模块在"+startDate+"~"+endDate+"期间没有访问数据</div>");
+        }else{
+            anaMind(data,module);
+        }
+    });
+}
+
+// 请求url上下游数据
+function ajaxUrlMind(url){
+    $("#jsmind_container").html("");
+    var startDate = $("#startDate").val();
+    var endDate = $("#endDate").val();
+    var param = {date:startDate+','+endDate,actionType:'url',actionName:encodeURI(url),format:'json',module:'API',method:'Transitions.getTransitionsForAction',filter_limit:-1,idSite:idSite,period:'range',token_auth:t};
+    ajax_jsonp(piwik_url,param,function(data){
+        data = eval(data);
+        if(data.hasOwnProperty("result") && data.result == "error"){
+            $("#jsmind_container").html("<div style='padding: 300px 0 0 400px;'>本页面在"+startDate+"~"+endDate+"期间没有访问数据</div>");
+        }else{
+            anaMind(data,url);
+        }
+    });
+}
+
+// 格式化页面上下游数据
+function anaMind(pageUpDownData,url){
+    var mind = {
+		/* 元数据，定义思维导图的名称、作者、版本等信息 */
+        "meta":{
+            "name":"pageUpDown-tree",
+            "author":"pud",
+            "version":"0.1"
+        },
+		/* 数据格式声明 */
+        "format":"node_tree",
+		/* 数据内容 */
+        "data":{}
+    };
+    var data = {};
+    var previousPages = null; // 来自内部页面
+    var website = null; // 来自网站
+    var direct = null; // 直接输入
+    var followingPages = null; // 转向站内页面
+    var outlinks = null; // 离站链接
+    var exits = null; // 退出页
+    var previousPagesTotal = 0;
+    var websiteTotal = 0;
+    var followingPagesTotal = 0;
+    var outlinksTotal = 0;
+    // 来自内部页面数据格式化
+    var previousPagesData = pageUpDownData['previousPages'];
+    if(previousPagesData.length > 0){
+        children = [];
+        for(var i in previousPagesData){
+            var row = previousPagesData[i];
+            previousPagesTotal += row.referrals;
+        }
+        for(var i in previousPagesData){
+            var row = previousPagesData[i];
+            var label = row.label;
+            var referrals = row.referrals;
+            var prop = (referrals/previousPagesTotal*100).toFixed(0);
+            topic = '<span title="'+label+'">'+label+'</span><br/><font size=2>'+referrals+' 次来自站内页面  占'+prop+'%</font>';
+            children.push({id:'previousPages'+i,topic:topic});
+        }
+        previousPages = {id:'previousPages',topic:'来自内部页面',direction:'left',expanded:true,children:children};
+    }
+    // 来自网站数据格式化
+    var referrersData = pageUpDownData['referrers'];
+    if(referrersData.length > 1){
+        var details = referrersData[1]['details'];
+        websiteTotal = referrersData[1].visits;
+        children = [];
+        for(var i in details){
+            var row = details[i];
+            var label = row.label;
+            var referrals = row.referrals;
+            var prop = (referrals/websiteTotal*100).toFixed(0);
+            topic = '<span title="'+label+'">'+label+'</span><br/><font size=2>'+referrals+' 次来自网站  占'+prop+'%</font>';
+            children.push({id:'website'+i,topic:topic});
+        }
+        website = {id:'website',topic:'来自网站',direction:'left',expanded:true,children:children};
+    }
+    // 直接输入格式化
+    var visits = referrersData[0].visits;
+    if(visits != 0){
+        direct = {id:'direct',topic:'直接访问	'+visits+'次',direction:'left'}
+    }
+    // 转向站内页面格式化
+    var followingPagesData = pageUpDownData['followingPages'];
+    if(followingPagesData.length > 0){
+        children = [];
+        for(var i in followingPagesData){
+            var row = followingPagesData[i];
+            followingPagesTotal += row.referrals;
+        }
+        for(var i in followingPagesData){
+            var row = followingPagesData[i];
+            var label = row.label;
+            var referrals = row.referrals;
+            var prop = (referrals/followingPagesTotal*100).toFixed(0);
+            topic = '<span title="'+label+'">'+label+'</span><br/><font size=2>'+referrals+' 次转向站内页面  占'+prop+'%</font>';
+            children.push({id:'followingPages'+i,topic:topic});
+        }
+        followingPages = {id:'followingPages',topic:'转向站内页面',direction:'right',expanded:true,children:children};
+    }
+    // 离站链接格式化
+    var outlinksData = pageUpDownData['outlinks'];
+    if(outlinksData.length > 0){
+        children = [];
+        for(var i in outlinksData){
+            var row = outlinksData[i];
+            outlinksTotal += row.referrals;
+        }
+        for(var i in outlinksData){
+            var row = outlinksData[i];
+            var label = row.label;
+            var referrals = row.referrals;
+            var prop = (referrals/outlinksTotal*100).toFixed(0);
+            topic = '<span title="'+label+'">'+label+'</span><br/><font size=2>'+referrals+' 次转向外部链接  占'+prop+'%</font>';
+            children.push({id:'outlinks'+i,topic:topic});
+        }
+        outlinks = {id:'outlinks',topic:'转向外部链接',direction:'right',expanded:true,children:children};
+    }
+    // 退出页格式化
+    var e = pageUpDownData['pageMetrics'].exits;
+    if(e != 0){
+        exits = {id:'exits',topic:'直接退出	'+e+'次',direction:'right'};
+    }
+    // 组装
+    var pv = pageUpDownData['pageMetrics'].pageviews; // 浏览量
+    var loops = pageUpDownData['pageMetrics'].loops; // 刷新次数
+    topic = '<font size=3>'+url+'<br/>&nbsp;&nbsp;'+pv+' 浏览量<hr style="margin:5px 0 5px 0"/>'
+        +'入口流量<br/>'
+        +'<font size=2>&nbsp;&nbsp;'+previousPagesTotal+' 次来自站内页面<br/>&nbsp;&nbsp;'+websiteTotal+' 次来自网站</br>&nbsp;&nbsp;'+visits+' 次来自直接访问<br/>&nbsp;&nbsp;'+loops+' 次刷新页面<br/></font>'
+        +'出口流量<br/>'
+        +'<font size=2>&nbsp;&nbsp;'+followingPagesTotal+' 次转向站内页面</br>&nbsp;&nbsp;'+outlinksTotal+' 次转向外部链接</br>&nbsp;&nbsp;'+e+' 次直接退出</font>'
+        +'</font>';
+    data = {"id":"root","topic":topic,"children":[]};
+    children = [];
+    if(previousPages != null){
+        children.push(previousPages);
+    }
+    if(website != null){
+        children.push(website);
+    }
+    if(direct != null){
+        children.push(direct);
+    }
+    if(followingPages != null){
+        children.push(followingPages);
+    }
+    if(outlinks != null){
+        children.push(outlinks);
+    }
+    if(exits != null){
+        children.push(exits);
+    }
+    data['children']=children;
+    mind['data'] = data;
+    showJsmind(mind);
+}
+
+// 展示jsmind图
+function showJsmind(mind){
+    var options = {
+        container:'jsmind_container',
+        editable:false,
+        theme:'info',
+        support_html : true,
+        view:{
+            hmargin:30,        // 思维导图距容器外框的最小水平距离
+            vmargin:50,         // 思维导图距容器外框的最小垂直距离
+            line_width:2,       // 思维导图线条的粗细
+            line_color:'#555'   // 思维导图线条的颜色
+        },
+    };
+    var jm = new jsMind(options);
+    jm.show(mind);
+}
+// 页面上下游end
